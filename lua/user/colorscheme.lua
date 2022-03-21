b9730c56bfd260b1716e28a798190fe114ae3985
Yin Wang <yin.wang@Yins-MacBook-Pro.local>
2022-03-20 20:27:29 -0400

    Change colortheme to nightfox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,17 @@
+local status_ok, nightfox = pcall(require, "nightfox")
+if not status_ok then
+  return
+end
+
+nightfox.setup({
+  options = {
+    dim_inactive = true,
+  }
+})
+
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme nightfox
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
